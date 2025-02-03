b6aa5f319d3fcceb5e7134876851baf05ec05089
MLindner0 <lindner_melanie@gmx.de>
2025-02-03 09:14:38 +0000

    adjust log in submission script

--- resume_nf.sh
+++ resume_nf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --comment=nextflow_slurm_trial
-#SBATCH --output=submission_logs/slurm_trial_2.log
+#SBATCH --comment=nextflow_slurm_b1.1
+#SBATCH --output=submission_logs/slurm_b1.1.log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
