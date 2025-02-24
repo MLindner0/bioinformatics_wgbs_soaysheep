b14b65ef6480e34d8e8b0f9fa80fdd3702919d18
MLindner0 <lindner_melanie@gmx.de>
2025-02-24 10:48:15 +0000

    fix issues with sample names for batch 3

--- launch_nf.sh
+++ launch_nf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --comment=nextflow_slurm_trial_2
-#SBATCH --output=submission_logs/slurm_trial_2.log
+#SBATCH --comment=nextflow_slurm_trial_b3
+#SBATCH --output=submission_logs/slurm_trial_b3.log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
